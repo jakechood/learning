89b737557d27f904795c8e52e48e4d09c0e534e5
Jake Sturgill <140123683+jakestu@users.noreply.github.com>
2023-12-21 10:10:07 -0600

    Edit and Add files to prep for assignment

---
+++ java_learning_projects/loops/GeneralizedHarmonic.java
@@ -0,0 +1,15 @@
+// GeneralizedHarmonic is a function that takes two integer command-line arguments n and r
+
+//and uses a for loop
+
+// to compute the nth generalize harmonic of order r
+
+public class GeneralizedHarmonic {
+    public static void main(int n, int r) {
+        for (int i = 0; i <= n; i++) {
+            a = 
+            
+
+        }
+    }
+}
