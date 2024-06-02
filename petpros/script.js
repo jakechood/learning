cb505e96bc3e789f766cc32b477ab71e261abb91
Jake Sturgill Hood <jakechood@gmail.com>
2024-06-02 12:47:04 -0500

    Increases prototype fidelity; integrates w3 layout framework

---
+++ petpros/script.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("pageForm").addEventListener("submit", function (event) {
+        var email = document.getElementById("emailIn").value;
+        var confEmail = document.getElementById("confEmail").value;
+
+        // Check if email fields match
+        if (email !== confEmail) {
+            document.getElementById("emailError").classList.remove("hidden");
+            event.preventDefault(); // Prevent form submission
+        } else {
+            document.getElementById("emailError").classList.add("hidden");
+        }
+
+        // Email validation
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            alert("Invalid email address");
+            event.preventDefault(); // Prevent form submission
+        }
+    });
+});
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function openMenu() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  };
+
+const popover = document.getElementById("myLinks");
+const toggleBtn = document.getElementById("navButton");
+
+// Check for popover API support.
+function supportsPopover() {
+  return HTMLElement.prototype.hasOwnProperty("popover");
+}
+
+if (supportsPopover()) {
+  // Set the <div> element to be an auto popover
+  popover.popover = "auto";
+  // Set the button popover target to be the popover
+  toggleBtn.popoverTargetElement = popover;
+
+  // Set that the button toggles popover visibility
+  toggleBtn.popoverTargetAction = "toggle";
+} else {
+  popover.textContent = "Popover API not supported.";
+  toggleBtn.hidden = true;
+}
