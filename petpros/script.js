81f3496a90936d54f3ec16881a145b4d4a2feabd
Jake Sturgill Hood <jakechood@gmail.com>
2024-06-11 08:53:55 -0500

    Approaches form functionality

--- petpros/script.js
+++ petpros/script.js
@@ -50,21 +50,12 @@ if (supportsPopover()) {
   toggleBtn.hidden = true;
 }
 
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function formState() {
-  var x = document.getElementByClass("pageForm");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-};
 var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
 function showTab(n) {
   // This function will display the specified tab of the form ...
-  var x = document.getElementsByClassName("pageForm");
+  var x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
   // ... and fix the Previous/Next buttons:
   if (n == 0) {
@@ -77,14 +68,13 @@ function showTab(n) {
   } else {
     document.getElementById("nextBtn").innerHTML = "Next";
   }
-  // ... and run a function that displays the correct step indicator:
-  fixStepIndicator(n)
 }
+
 function nextPrev(n) {
   // This function will figure out which tab to display
-  var x = document.getElementsByClassName("pageForm");
+  var x = document.getElementsByClassName("tab");
   // Exit the function if any field in the current tab is invalid:
-  if (n == 1 && !validateForm()) return false;
+  if (n == 1) return false;
   // Hide the current tab:
   x[currentTab].style.display = "none";
   // Increase or decrease the current tab by 1:
@@ -92,7 +82,7 @@ function nextPrev(n) {
   // if you have reached the end of the form... :
   if (currentTab >= x.length) {
     //...the form gets submitted:
-    document.getElementById("regForm").submit();
+    document.getElementById("pageForm").submit();
     return false;
   }
   // Otherwise, display the correct tab:
